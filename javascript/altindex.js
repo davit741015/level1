897f074aff247a93fb5fcfe4913f418e635a0aa7
davit741015 <davidsonario671@gmail.com>
2025-03-15 15:40:02 +0400

    init Replvl1

--- javascript/altindex.js
+++ javascript/altindex.js
@@ -1,19 +1,18 @@
-
-  const increaseBtn = document.getElementById("increaseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const countLabel = document.getElementById("countLabel");
 let count = 0;
 
-increaseBtn.onclick = function(){
+increaseBtn.onclick = function () {
     count++;
-    countLabel.textContent = count;
+    countLabel.innerHTML = count;
 }
-decreaseBtn.onclick = function(){
+decreaseBtn.onclick = function () {
     count--;
     countLabel.textContent = count;
 }
-resetBtn.onclick = function(){
+resetBtn.onclick = function () {
     count = 0;
     countLabel.textContent = count;
 }
